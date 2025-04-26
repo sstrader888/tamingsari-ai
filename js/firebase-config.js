25c9441f860264ec3e359f9e28dc0359b0c9dc71
sstrader888 <harrynurul152489@gmail.com>
2025-04-26 21:02:51 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDpOhB_wvp3DHbWMuROQDnHR8x8LTCNDng",
-  authDomain: "sstrader-affiliate.firebaseapp.com",
-  projectId: "sstrader-affiliate",
-  storageBucket: "sstrader-affiliate.appspot.com",
-  messagingSenderId: "859244663056",
-  appId: "1:859244663056:web:4fbad1a3655ff85f499595",
-};
+const firebaseConfig = {
+  apiKey: "AIzaSyCi5tALvmSyxXTDTNIP79Dvbh95awlkMno",
+  authDomain: "sstrader-wallet.firebaseapp.com",
+  projectId: "sstrader-wallet",
+  storageBucket: "sstrader-wallet.firebasestorage.app",
+  messagingSenderId: "878953885578",
+  appId: "1:878953885578:web:29acc35bb145bebbd0a273",
+  measurementId: "G-R22W5E4SK5"
