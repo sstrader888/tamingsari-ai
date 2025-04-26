7e5c06826a74d024800252d48957547c2bd84af3
sstrader888 <harrynurul152489@gmail.com>
2025-04-26 20:37:21 +0800

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// Ganti dengan Firebase config cikgu
+export const firebaseConfig = {
+  apiKey: "AIzaSyDpOhB_wvp3DHbWMuROQDnHR8x8LTCNDng",
+  authDomain: "sstrader-affiliate.firebaseapp.com",
+  projectId: "sstrader-affiliate",
+  storageBucket: "sstrader-affiliate.appspot.com",
+  messagingSenderId: "859244663056",
+  appId: "1:859244663056:web:4fbad1a3655ff85f499595",
+};
